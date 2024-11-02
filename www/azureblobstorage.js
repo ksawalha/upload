30e698aee018413645380ccacf597041fb03fbdb
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2024-11-02 21:44:59 +0300

    Update azureblobstorage.js

--- www/azureblobstorage.js
+++ www/azureblobstorage.js
@@ -1,33 +1,15 @@
-var AzureBlobStorage = {
-    /**
-     * Upload files from device storage to Azure Blob Storage.
-     * @param {Array} files - Array of file objects containing path and name.
-     * @param {Function} successCallback - Callback function invoked on successful upload.
-     * @param {Function} errorCallback - Callback function invoked on upload error.
-     */
-    uploadDeviceFiles: function(files, successCallback, errorCallback) {
-        // Check if files are provided
-        if (!Array.isArray(files) || files.length === 0) {
-            errorCallback("No files provided for upload.");
-            return;
-        }
+var exec = require('cordova/exec');
 
-        // Construct an array of file objects
-        var fileArray = files.map(function(file) {
-            return {
-                path: file.path, // The local file path
-                name: file.name  // The file name
-            };
-        });
-
-        // Call the native plugin
-        cordova.exec(successCallback, errorCallback, 'AzureBlobStoragePlugin', 'uploadDeviceFiles', [fileArray]);
+var AzureBlobStoragePlugin = {
+    uploadDeviceFiles: function(files, sasToken, containerName, successCallback, errorCallback) {
+        var params = {
+            files: files,
+            sasToken: sasToken,
+            containerName: containerName
+        };
+        
+        exec(successCallback, errorCallback, 'AzureBlobStoragePlugin', 'uploadDeviceFiles', [params]);
     }
 };
 
-// Export the AzureBlobStorage object for external use
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = AzureBlobStorage;
-} else {
-    window.AzureBlobStorage = AzureBlobStorage;
-}
+module.exports = AzureBlobStoragePlugin;
